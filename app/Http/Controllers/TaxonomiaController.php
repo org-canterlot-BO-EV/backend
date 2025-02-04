a9c8f1ae8f2d4e90945f8561b8574779219e4215
bHannah <borsikaaa1@gmail.com>
2025-02-04 11:55:59 +0100

    végpontok

--- app/Http/Controllers/TaxonomiaController.php
+++ app/Http/Controllers/TaxonomiaController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaxonomiaController extends Controller
 {
-    //
+    public function mindenTaxonomia(){
+        $tax = DB::table('taxonomias')
+        ->select('taxonomia_id', 'elnevezes')
+        ->get();
+        return $tax;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\ProgramTipusController;
+use App\Http\Controllers\TaxonomiaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,4 @@ Route::get('/programtipusok', [ProgramTipusController::class, "mindenProgramTipu
 Route::get('/felhasznalok', [UserController::class, "mindenFelhasznalo"]);
 Route::get('/programok', [ProgramController::class, "mindenProgram"]);
 Route::post('/program-hozzadasa', [UserController::class, "store"]);
+Route::get('/mindentaxonomia', [TaxonomiaController::class, "mindenTaxonomia"]);
